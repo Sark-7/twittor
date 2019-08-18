73d633124c99b1406ab4d7dc59fd9f972f9ef973
Sark-7 <54245164+Sark-7@users.noreply.github.com>
2019-08-18 20:32:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,17 +6,17 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
-    'twittor/',
-    'index.html',
-    'css/style.css',
-    'img/favicon.ico',
-    'img/avatars/hulk.jpg',
-    'img/avatars/ironman.jpg',
-    'img/avatars/spiderman.jpg',
-    'img/avatars/thor.jpg',
-    'img/avatars/wolverine.jpg',
-    'js/app.js',
-    'js/sw-utils.js',
+//     'twittor/',
+//     'index.html',
+//     'css/style.css',
+//     'img/favicon.ico',
+//     'img/avatars/hulk.jpg',
+//     'img/avatars/ironman.jpg',
+//     'img/avatars/spiderman.jpg',
+//     'img/avatars/thor.jpg',
+//     'img/avatars/wolverine.jpg',
+//     'js/app.js',
+//     'js/sw-utils.js',
 ];
 
 const APP_SHELL_INMUTABLE = [
